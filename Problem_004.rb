708c7bcc9adb6b48f1843b59e66695b1bf700811
LesRedfield <les.redfield.dev@gmail.com>
2015-08-16 14:02:34 -0400

    Update Problem_004.rb

--- Problem_004.rb
+++ Problem_004.rb
@@ -2,3 +2,18 @@
 #The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
+pal = 0
+
+x = 100
+
+while x <= 999
+  for i in (100..999)
+    p = x*i
+    if (p > pal) && (p.to_s == p.to_s.reverse)
+      pal = p
+    end
+  end
+  x += 1
+end
+
+puts pal
